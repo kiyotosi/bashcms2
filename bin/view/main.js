8c6fa0f2dccd04660b8865a85c1a2e5d54b7e95c
kiyotosi <kiyotosigm@gmail.com>
2021-12-01 21:32:07 +0900

    5.2

---
+++ bin/view/main.js
@@ -0,0 +1,31 @@
+window.onload = function () {
+	lastArticles(10);
+	linkKeywords();
+}
+
+function lastArticles(num){
+	var httpReq = new XMLHttpRequest();
+	httpReq.onreadystatechange = function(){
+		if(httpReq.readyState != 4 || httpReq.status != 200)
+			return;
+
+		document.getElementById("last-articles").innerHTML = httpReq.responseText;
+	}
+	var url = "/last_articles.cgi?num=" + num;
+	httpReq.open("GET",url,true);
+	httpReq.send(null);
+}
+
+function linkKeywords(){
+	var httpReq = new XMLHttpRequest();
+	httpReq.onreadystatechange = function(){
+		if(httpReq.readyState != 4 || httpReq.status != 200)
+			return;
+
+		document.getElementById("keywords").innerHTML = httpReq.responseText;
+	}
+	var word = document.getElementById("keywords").innerHTML;
+	var url = "/link_keywords.cgi?keywords=" + encodeURIComponent(word);
+	httpReq.open("GET",url,true);
+	httpReq.send(null);
+}
